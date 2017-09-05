b51cf82e324b9084657f30e12d57b5e12baef2c0
kyungloak <kwunglock@gmail.com>
2017-09-05 13:03:54 +0900

    commit test

---
+++ app/src/main/java/com/example/roak/myapplication/commit_test.java
@@ -0,0 +1,11 @@
+package com.example.roak.myapplication;
+
+import android.app.Activity;
+
+/**
+ * Created by roak on 2017-09-05.
+ */
+
+public class commit_test extends Activity {
+    private static final String TAG = "MainActivity";
+}
